42d11d6bed87fd16ac6f9c7c4785975e6a329b58
Bon Tertius Tuyishimire <tuyishimirebt12@gmail.com>
2024-12-26 14:45:51 +0200

    .

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'http://localhost:8080/api',
-  baseURL: 'https://restaurant-ordering-system-a6tt.onrender.com/api',
+  baseURL: 'http://localhost:8080/api',
+  // baseURL: 'https://restaurant-ordering-system-a6tt.onrender.com/api',
 });
 
 export const auth = {
